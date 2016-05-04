ae1c17c0e8a2c272ce502f3e550a1f1915f6e622
Mallory Payne <mallory.payne18@gmail.com>
2016-05-04 16:36:28 -0400

    almost got the page authors to appear

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ var express = require('express');
 var app = express();
 var morgan = require('morgan');
 var swig = require('swig');
-var routes = require('./routes/route.js');
+var wikiRouter = require('./routes/wiki.js');
+var userRouter = require('./routes/users.js')
 var path = require('path');
 var bodyParser = require('body-parser');
+var models = require('./models/');
+var Page = models.Page; 
+var User = models.User; 
 
 app.set('views', path.join(__dirname, '/views/')); // where to find the views
 app.set('view engine', 'html'); // what file extension do our templates have
@@ -20,8 +24,27 @@ app.use(morgan('tiny'));
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.listen(3000, function(){
-	console.log('listening on port 3000');
+
+
+models.Page.sync()
+.then(function(){
+	models.User.sync();
+})
+.then(function () {
+	app.listen(3000, function(){
+		console.log('listening on port 3000');
+	});
+})
+.catch(console.error);
+
+app.get('/', function(req, res, next){
+	Page.findAll()
+	.then(function(pages){
+		res.render('index', {
+			pages: pages
+		});
+	}).catch(console.error);
 });
 
-app.use('/', routes);
+app.use('/wiki', wikiRouter);
+app.use('/users', userRouter);
--- db.js
+++
@@ -1,3 +0,0 @@
-// var pg = require('pg');
-// var conString = 'postgres://localhost:5432/twitterdb';
-// var client = new pg.Client(conString);
